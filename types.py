1658b73749847badc3a7edc60bba0c8ffcebd685
Evgeniy <e.pogrebnyak@gmail.com>
2020-04-19 01:17:25 +0300

    добавлены функции апроксимации

--- types.py
+++ types.py
@@ -4,17 +4,30 @@
 
   [Trip]
   
-Результат - перекрытие треков в паре (два числа от 0 до 1)
+Результат - список словарей, характеризующих пары треков, пример:
     
-  [(int, int, float, float)]
+  {'track_1': 0,
+   'track_2': 7,
+   'min_dist': 0.132,
+   'max_dist': 85.604,
+   'cov_1': 0.77,
+   'cov_2': 0.26}
 
-Перекрытие - часть трека, которая находится на расстоянии не более
+'track_1' и 'track_2' - номеры треков из выборки.
+
+'min_dist' и 'max_dist' - расстояния:
+             минимальное и максимальное расстояние между фигурами 
+             треков.
+
+'cov_1' и 'cov_2' - перекрытие:
+             часть трека, которая находится на расстоянии не более
              заданного радиуса от любой точки другого трека в паре 
              сравнения.
 
 """
 import datetime
 from dataclasses import dataclass
+from typing import List, Tuple, Generator
 
 import pandas as pd
 import numpy as np
@@ -22,7 +35,12 @@ import numpy as np
 from geopy.distance import great_circle
 from scipy.spatial.distance import cdist
 
+
 pd.set_option("mode.chained_assignment", None)
+
+
+class Route(pd.DataFrame):
+    pass
 
 
 @dataclass
@@ -32,13 +50,13 @@ class Trip:
     route: pd.DataFrame
 
 
-def combinations(n: int) -> (int, int):
+def get_combinations(n: int) -> List[Tuple[int, int]]:
     from itertools import combinations
 
     return [(i, j) for i, j in combinations(range(n), 2)]
 
 
-assert len(combinations(49)) == 1176
+# Импорт данных
 
 
 def check_incoming_dataframe(df: pd.DataFrame):
@@ -48,7 +66,7 @@ def check_incoming_dataframe(df: pd.DataFrame):
         raise ValueError(f"Missing from {df.head()}: {missing_columns}")
 
 
-def yield_trips(df: pd.DataFrame) -> [Trip]:
+def yield_trips(df: pd.DataFrame) -> Generator[Trip, None, None]:
     check_incoming_dataframe(df)
     df["date"] = df["time"].apply(lambda x: x.date())
     for date in df.date.unique():
@@ -57,13 +75,16 @@ def yield_trips(df: pd.DataFrame) -> [Trip]:
             yield Trip(car, date, route(df[ix]))
 
 
-def route(df: pd.DataFrame) -> pd.DataFrame:
+# Работа с треком
+
+
+def route(df: pd.DataFrame) -> Route:
     res = df[["time"]]
     res["coord"] = list(zip(df.lat, df.lon))
     return res.sort_values("time")
 
 
-def seconds(series):
+def seconds(series: pd.Series) -> pd.Series:
     return series.apply(lambda x: x.total_seconds())
 
 
@@ -75,17 +96,24 @@ def time_deltas(df: pd.DataFrame) -> pd.DataFrame:
     return seconds(df.time.diff())
 
 
-def milage(df: pd.DataFrame) -> pd.DataFrame:
-    return dist_delta(df).cumsum().fillna(0)
-
-
 def dist_delta(df: pd.DataFrame) -> pd.DataFrame:
     df["prev_coord"] = df["coord"].shift(1)
     return df.apply(lambda x: safe_distance(x.coord, x.prev_coord), axis=1)
 
 
+def milage(df: pd.DataFrame) -> pd.DataFrame:
+    return dist_delta(df).cumsum().fillna(0)
+
+
+def points(route_df: Route) -> np.ndarray:
+    return np.array(route_df.coord.to_list())
+
+
+# Функции расстояний
+
+
 def distance_km(a: tuple, b: tuple):
-    # great_circle - менее точен, но быстрее чем geopy.distance.distance
+    # great_circle() менее точен, но быстрее чем geopy.distance.distance
     # https://geopy.readthedocs.io/en/stable/#module-geopy.distance
     return great_circle(a, b).km
 
@@ -108,6 +136,13 @@ def safe_distance_2(a, b):
         return distance_km(a, b)
 
 
+# Функции для апроксимации трека:
+#  - [x] по времени в пути (инкремент, количество сегментов)
+#  - [x] по расстоянию (инкремент, количество сегментов)
+#  - [ ] по астрономическому времени (есть в ноутбке)
+#  - [ ] по остановкам (есть в "тяжелом" алгоритме)
+
+
 def growing_index(xs, step):
     xs = [x for x in xs]
     result = [0]  # will include start point
@@ -124,25 +159,89 @@ def growing_index(xs, step):
     return result
 
 
-def time_step(route_df, minutes=60):
-    xs = duration(route_df)
-    ix = growing_index(xs, minutes)
-    return route_df.iloc[ix]
+def time_increment(minutes):
+    def accept(route_df):
+        xs = duration(route_df)
+        ix = growing_index(xs, minutes)
+        return route_df.iloc[ix]
+
+    return accept
 
 
-def distance_step(route_df, km=5):
-    xs = milage(route_df)
-    ix = growing_index(xs, km)
-    return route_df.iloc[ix]
+def distance_increment(km):
+    def accept(route_df):
+        xs = milage(route_df)
+        ix = growing_index(xs, km)
+        return route_df.iloc[ix]
+
+    return accept
 
 
-def points(route_df) -> np.ndarray:
-    return np.array(route_df.coord.to_list())
+def find_index_quantile(xs, q):
+    if q == 0:
+        return 0
+    if q == 1:
+        return -1
+    return np.searchsorted(xs, v=np.quantile(xs, q), side="left")
+
+
+def find_index(xs, n_segments):
+    qs = np.linspace(0, 1, n_segments + 1)
+    return [find_index_quantile(xs, q) for q in qs]
+
+
+def n_segments_by_distance(n):
+    def accept(route_df):
+        xs = milage(route_df)
+        ix = find_index(xs, n)
+        return route_df.iloc[ix]
+
+    return accept
+
+
+def n_segments_by_time(n):
+    def accept(route_df):
+        xs = duration(route_df)
+        ix = find_index(xs, n)
+        return route_df.iloc[ix]
+
+    return accept
+
+
+# Матрица расстояний между точками двух треков
 
 
 def proximity(r1, r2):
     mat = cdist(points(r1), points(r2), safe_distance_2)
     return Proximity(mat)
+
+
+@dataclass
+class Proximity:
+    """Матрица расстояний между двумя треками."""
+
+    mat: np.ndarray
+
+    def min(self):
+        """
+    Минимиальное расстояние между всеми точками двух треков.
+    """
+        return round(np.nanmin(self.mat), 3)
+
+    def max(self):
+        """
+    Максимальное расстояние между всеми точками двух треков.
+    """
+        return round(np.amax(self.mat), 3)
+
+    def side_min(self, axis: int):
+        # 1 is to search by columns - applies to searching minima for 1st route
+        # 0 is to search by rows - applies to searching minima for 2nd route
+        x = np.nanmin(self.mat, axis)
+        return x[~np.isnan(x)]
+
+    def minimal_distances(self):
+        return Coverage(self.side_min(1)), Coverage(self.side_min(0))
 
 
 @dataclass
@@ -202,71 +301,61 @@ class Coverage:
         """Коэффициент перекрытия - доля трека, которая находится 
            на расстоянии не более заданного радиуса от другого трека.
         """
-        return round(len(self.in_proximity(radius)) / len(self.mins), 3)
-
-
-@dataclass
-class Proximity:
-    """Матрица расстояний между двумя треками."""
-
-    mat: np.ndarray
-
-    def min(self):
-        """
-    Минимиальное расстояние между всеми точками двух треков.
-    """
-        return np.nanmin(self.mat)
-
-    def max(self):
-        """
-    Максимальное расстояние между всеми точками двух треков.
-    """
-        return np.amax(self.mat)
-
-    def side_min(self, axis: int):
-        # 1 is to search by columns - applies to searching minima for 1st route
-        # 0 is to search by rows - applies to searching minima for 2nd route
-        x = np.nanmin(self.mat, axis)
-        return x[~np.isnan(x)]
-
-    def minimal_distances(self):
-        return Coverage(self.side_min(1)), Coverage(self.side_min(0))
-
-
-def members(pairs):
-    return [p for pair in pairs for p in pair]
+        return round(len(self.in_proximity(radius)) / len(self.mins), 2)
 
 
 def search(
     trips,
-    approximate_1=lambda df: time_step(df, minutes=60),
-    radius_1=5,
-    approximate_2=lambda df: distance_step(df, km=2.5),
-    radius_2=2.5 * 1.2,
+    approximate_with=[n_segments_by_distance(n=10), distance_increment(km=2.5),],
+    radius=[5, 2.5 * 1.2],
     limit=None,
 ):
+    """
+
+    Parameters
+    ----------
+    trips : [Trip]
+        Список поездок.
+    approximate_with : [Route -> Route]
+        
+    radius :  [float]
+        Радиусы для проверки сближения треков.
+    limit : int
+        Обработать *limit* первых пар, используется для тестирования.
+
+    Yields
+    ------
+    dict
+
+    """
+
+    f, g = approximate_with
+    radius_1, radius_2 = radius
 
     # Грубая апроксимация треков
-    routes = [approximate_1(t.route) for t in trips]
-    print("Simplified routes")
+    routes = [f(t.route) for t in trips]
+    print(f"Simplified {len(trips)} routes")
 
     def prox(tup: tuple):  # замыкание для доступа к routes
         i, j = tup
         return proximity(routes[i], routes[j])
 
     # Выбор пересекающися пар
-    pairs = [p for p in combinations(len(trips)) if prox(p).min() < radius_1]
+    pairs = [p for p in get_combinations(len(trips)) if prox(p).min() < radius_1]
     print(f"Found {len(pairs)} pairs of intersecting routes")
-    print("Refining approximation for candidate routes...")
+    print("Refining approximation for intersected routes...")
 
-    # урезаем для демо-примеров
+    # Урезаем для демо-примеров
     if limit:
         pairs = pairs[0:limit]
+        print(f"Trimmed dataset to {limit} pairs")
 
     # Уточняем апроксимацию треков
-    for i in members(pairs):
-        routes[i] = approximate_2(trips[i].route)
-    print(f"Made better approximation of {len(members(pairs))} routes")
+    from itertools import chain
+
+    for k, i in enumerate(set(chain.from_iterable(pairs))):
+        routes[i] = g(trips[i].route)
+    print(f"Made better approximation of {k} routes")
     print("Calculating distances between routes and reporting...")
 
     # Считаем перекрытие треков в пересекающися парах
@@ -275,14 +364,19 @@ def search(
         p = proximity(routes[i], routes[j])
         if p.min() < radius_1:
             md1, md2 = p.minimal_distances()
-            yield (i, j, p.min(), md1.coverage(radius_2), md2.coverage(radius_2))
-
-
-def pct(x: float) -> str:
-    return f"{x*100:.0f}%"
+            yield dict(
+                track_1=i,
+                track_2=j,
+                min_dist=p.min(),
+                max_dist=p.max(),
+                cov_1=md1.coverage(radius_2),
+                cov_2=md2.coverage(radius_2),
+            )
 
 
 if __name__ == "__main__":
+    # Получаем данные
+    assert len(get_combinations(49)) == 1176
 
     def get_dataframe():
         return pd.read_csv("one_day.zip", parse_dates=["time"])
@@ -293,25 +387,39 @@ if __name__ == "__main__":
 
     trips = list(yield_trips(df))
     assert len(trips) == 49
-    print(f"Created list of {len(trips) } trips")
+    print(f"Created list of {len(trips)} trips")
 
-    results = list(search(trips, limit=10))
-    for (i, j, m, c1, c2) in results:
-        print(i, j, round(m, 2), pct(c1), pct(c2))
+    # Запускаем алгоритм
+    results_ = list(search(trips, limit=None))
 
-    """
-    Фукнция раздить на n сегментов по расстоянию, по времени
+    results = (
+        pd.DataFrame(results_)
+        .assign(cov_total=lambda r: r.cov_1 + r.cov_2)
+        .sort_values("cov_total", ascending=False)
+        .reset_index()
+        .head(10)
+    )
+    print(results)
+    results.to_csv("output.csv", index=None)
+
+    """        
+    План перенести из ноутбука
+    -------------------------
     
-    Функции для апроксимации треков
-       - равные расстояния
-       - доля пробега
-       - по времени
-       - c остановками
+    Визуализация:          
         
-    Визуализация   
-       - отрисовка треков на карте     
+      - [ ] отрисовка треков на карте     
+      - [ ] прочие
        
-    Фильтры:
-        - по направлению
-        - по времени        
+    Фильтры:        
+        
+      - [ ] по направлению
+      - [ ] по времени         
+      
+    Соображения
+    -----------
+    
+    - в функциях упрощения много повторов кода, можно рефакторить
+    - для пробега нам нужно перезапускть duration() - много расчетов 
+    
     """
