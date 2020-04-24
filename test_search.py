57722fc27a3ac90ca651503e77ac2a920c707e9f
Evgeniy <e.pogrebnyak@gmail.com>
2020-04-24 11:24:33 +0300

    moved scripts out of master branch
    See legacy_scripts branch

--- test_search.py
+++
@@ -1,5 +0,0 @@
-from search import get_combinations
-
-
-def test_get_combinations():
-    assert len(get_combinations(49)) == 1176
--- test_visual.py
+++
@@ -1,5 +0,0 @@
-from visual import plot_two
-
-
-def test_plot_two(trips):
-    plot_two(trips, 10, 20)
