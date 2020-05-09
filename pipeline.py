45d8cbeb9939550c5b1dd8cce91a9c05d3c09bb8
E Pogrebnyak <e.pogrebnyak@gmail.com>
2020-05-10 02:08:56 +0300

    only 3 tracks

--- pipeline.py
+++ pipeline.py
@@ -21,21 +21,20 @@ def default_results(df, limit=None):
 
 
 def subset(full_csv, summaries_csv, days, types):
-    df_full = read_dataframe(full_csv)
-    vehicle_type = wrap_vehicle_type(summaries_csv)
-    subset_df = df_full
+    subset_df = read_dataframe(full_csv)
     if days:
-        subset_df = subset_by_dates(df_full, days)
+        subset_df = subset_by_dates(subset_df, days)
     if types:
+        vehicle_type = wrap_vehicle_type(summaries_csv)
         subset_df = subset_by_vehicle_types(subset_df, types, vehicle_type)
     return subset_df
 
 
 def pipeline(
-    url,
-    days,
-    types,
+    url,    
     json_folder,
+    days=None,
+    types=None,
     limit=None,
     filename_full_csv="df_full.csv",
     filename_summaries_csv="summaries.csv",
@@ -50,9 +49,9 @@ def pipeline(
 
 job = dict(
     url="https://github.com/epogrebnyak/rides-minimal/raw/master/sample_jsons/sample_jsons.zip",
+    json_folder="data2/json",
     days=None,
     types=None,
-    json_folder="data2/json",
     limit=None,
     filename_full_csv="data2/df_full.csv",
     filename_summaries_csv="data2/summaries.csv",
