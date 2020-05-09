136c54a207dbfcb5bdffe8e451678431863fd898
E Pogrebnyak <e.pogrebnyak@gmail.com>
2020-05-09 17:50:56 +0300

    Update __init__.py

--- rider/__init__.py
+++ rider/__init__.py
@@ -3,7 +3,6 @@ from .vehicles import wrap_vehicle_type
 from .dataframe import get_dataframe, subset, result_dataframe
 from .routes import (
     get_trips_and_routes,
-    milage,
     trips_dataframe,
     n_segments_by_distance,
 )
